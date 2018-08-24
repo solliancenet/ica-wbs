16ec0097cb983268a6240f748aeea9496b4ae9f5
Joel Hulen <joelhulen@gmail.com>
2018-08-24 02:41:52 -0400

    Added images

--- Starter-artifacts/Resources/data-migration-starter/media/starter-project/NorthwindMVC/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NorthwindMVC.MvcApplication" Language="C#" %>
--- Starter-artifacts/Resources/data-migration-starter/media/starter-project/NorthwindMVC/Models/SalesByYearViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NorthwindMVC.Models
-{
-    public class SalesByYearViewModel
-    {
-        public string Year { get; set; }
-
-        public int Count { get; set; }
-    }
-}
